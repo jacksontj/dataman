ff43cad6fe1d104cb740b54f1bf5c1a74ebb2deb
Thomas Jackson <jacksontj.89@gmail.com>
2017-05-03 10:08:08 -0700

    Add more hashing algos

--- src/router_node/sharding/hash.go
+++ src/router_node/sharding/hash.go
@@ -1,6 +1,11 @@
 package sharding
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/binary"
 	"fmt"
 	"reflect"
 	"strconv"
@@ -12,15 +17,28 @@ type HashFunc func(interface{}) (uint64, error)
 type HashMethod string
 
 const (
-	Cast HashMethod = "cast"
+	Cast   HashMethod = "cast"
+	MD5               = "md5"
+	SHA1              = "sha1"
+	SHA256            = "sha256"
+	SHA512            = "sha512"
+
 	// TODO: other hashing algos
-	//MD5             = "md5"
 )
 
 func (h HashMethod) Get() HashFunc {
 	switch h {
 	case Cast:
 		return CastFunc
+	case MD5:
+		return MD5Func
+	case SHA1:
+		return SHA1Func
+	case SHA256:
+		return SHA256Func
+	case SHA512:
+		return SHA512Func
+
 	default:
 		return nil
 	}
@@ -43,4 +61,56 @@ func CastFunc(key interface{}) (uint64, error) {
 		return 0, fmt.Errorf("Unable to typecast %v %v to uint64", typedKey, key)
 	}
 
+}
+
+func MD5Func(key interface{}) (uint64, error) {
+	switch typedKey := key.(type) {
+	case string:
+		sum := md5.Sum([]byte(typedKey))
+		var buf []byte
+		buf = sum[:]
+		return binary.LittleEndian.Uint64(buf), nil
+	default:
+		fmt.Println(reflect.TypeOf(key))
+		return 0, fmt.Errorf("Unable to typecast %v %v to uint64", typedKey, key)
+	}
+}
+
+func SHA1Func(key interface{}) (uint64, error) {
+	switch typedKey := key.(type) {
+	case string:
+		sum := sha1.Sum([]byte(typedKey))
+		var buf []byte
+		buf = sum[:]
+		return binary.LittleEndian.Uint64(buf), nil
+	default:
+		fmt.Println(reflect.TypeOf(key))
+		return 0, fmt.Errorf("Unable to typecast %v %v to uint64", typedKey, key)
+	}
+}
+
+func SHA256Func(key interface{}) (uint64, error) {
+	switch typedKey := key.(type) {
+	case string:
+		sum := sha256.Sum256([]byte(typedKey))
+		var buf []byte
+		buf = sum[:]
+		return binary.LittleEndian.Uint64(buf), nil
+	default:
+		fmt.Println(reflect.TypeOf(key))
+		return 0, fmt.Errorf("Unable to typecast %v %v to uint64", typedKey, key)
+	}
+}
+
+func SHA512Func(key interface{}) (uint64, error) {
+	switch typedKey := key.(type) {
+	case string:
+		sum := sha512.Sum512([]byte(typedKey))
+		var buf []byte
+		buf = sum[:]
+		return binary.LittleEndian.Uint64(buf), nil
+	default:
+		fmt.Println(reflect.TypeOf(key))
+		return 0, fmt.Errorf("Unable to typecast %v %v to uint64", typedKey, key)
+	}
 }
