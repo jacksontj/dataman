24242869ad8e33abca601771cc9cdc2afa0ab308
Thomas Jackson <jacksontj.89@gmail.com>
2017-06-12 09:54:03 -0700

    Remove unused examples (pulled from DB now)

--- src/storage_node/metadata/field_type_internal.go
+++ src/storage_node/metadata/field_type_internal.go
@@ -26,30 +26,5 @@ func listInternalFieldTypes() []*FieldType {
 			Name:        "_text",
 			DatamanType: Text,
 		},
-
-		/*
-			// TODO: move out to database?
-			&FieldType{
-				Name:        "age",
-				DatamanType: Int,
-				Constraints: []*ConstraintInstance{
-					&ConstraintInstance{
-						Type: LessThan,
-						Args: map[string]interface{}{"value": 200},
-					},
-				},
-			},
-
-			&FieldType{
-				Name:        "phone number",
-				DatamanType: String,
-				Constraints: []*ConstraintInstance{
-					&ConstraintInstance{
-						Type: LessThanEqual,
-						Args: map[string]interface{}{"value": 10},
-					},
-				},
-			},
-		*/
 	}
 }
