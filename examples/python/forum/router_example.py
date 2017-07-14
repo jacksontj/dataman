be1c29af7bf714339018bc8843c206e93b2f1e7a
Thomas Jackson <jacksontj.89@gmail.com>
2017-07-14 08:18:24 -0700

    Update to match new numbers etc.

--- examples/python/forum/router_example.py
+++ examples/python/forum/router_example.py
@@ -41,12 +41,12 @@ def ensure_datastore(urlbase):
                 "shards": [
                     {
                         "shard_instance": 1,
-                        "datastore_shard_id": 70,
+                        "datastore_shard_id": 72,
                         "provision_state": 3
                     },
                     {
                       "shard_instance": 2,
-                      "datastore_shard_id": 71,
+                      "datastore_shard_id": 73,
                       "provision_state": 3
                     }
                 ],
