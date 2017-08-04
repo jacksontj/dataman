b3e7056c17fba9c04bc10edf657bca1ebc82955d
Thomas Jackson <jacksontj.89@gmail.com>
2017-08-04 15:16:08 -0700

    More detail in comment

--- src/router_node/client.go
+++ src/router_node/client.go
@@ -8,7 +8,8 @@ import (
 	"github.com/jacksontj/dataman/src/router_node/metadata"
 )
 
-// TODO: remove this method? Doesn't do much
+// TODO: remove this method? Doesn't do much. Once we support sending things to more than just the primary
+// this won't be helpful (since each call will need to know what is acceptable)
 // TODO: use same client as everyone else (with some LRU/LFU cache of the connections?)
 // Take a query and send it to a given destination
 func Query(ctx context.Context, clientManager clientmanager.ClientManager, datasourceInstance *metadata.DatasourceInstance, q *query.Query) (*query.Result, error) {
