391b3d121f295646dbcf02ae46a401d68482b32e
Thomas Jackson <jacksontj.89@gmail.com>
2018-01-17 14:00:19 -0800

    Add some Value serialziation tests

--- src/storage_node/datasource/pgstore/util_test.go
+++ src/storage_node/datasource/pgstore/util_test.go
@@ -2,6 +2,8 @@ package pgstorage
 
 import (
 	"testing"
+
+	"github.com/jacksontj/dataman/src/datamantype"
 )
 
 func TestCollectionFieldToSelector(t *testing.T) {
@@ -25,5 +27,44 @@ func TestCollectionFieldToSelector(t *testing.T) {
 			t.Fatalf("Mismatch in %d expected=%v actual=%v", i, test.Output, ret)
 		}
 	}
+}
 
+func TestValueSerialization(t *testing.T) {
+	tests := []struct {
+		Type   datamantype.DatamanType
+		Input  interface{}
+		Output string
+	}{
+		{
+			datamantype.Int,
+			int(1),
+			"'1'",
+		},
+		{
+			datamantype.Int,
+			float64(1),
+			"'1'",
+		},
+		{
+			datamantype.Int,
+			int64(1),
+			"'1'",
+		},
+		{
+			datamantype.Int,
+			uint64(1),
+			"'1'",
+		},
+	}
+
+	for _, test := range tests {
+		ret, err := serializeValue(test.Type, test.Input)
+		if err != nil {
+			t.Fatalf("Error: %v", err)
+		}
+
+		if ret != test.Output {
+			t.Fatalf("Mismatched output: expected=%s actual=%s", test.Output, ret)
+		}
+	}
 }
