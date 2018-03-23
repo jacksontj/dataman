170b5c4661b580e438d5c0c931d332dbf3151d83
Thomas Jackson <jacksontj.89@gmail.com>
2018-03-23 11:31:42 -0700

    Cleanup

--- src/query/query.go
+++ src/query/query.go
@@ -1,31 +1,7 @@
 package query
 
+// QueryType is the list of all query functions dataman supports
 type QueryType string
-
-// TODO: func to validate the mix of arguments
-type QueryArgs struct {
-	// Shared options
-	DB            string `json:"db"`
-	Collection    string `json:"collection"`
-	ShardInstance string `json:"shard_instance,omitempty"`
-
-	Fields []string `json:"fields"`
-	Sort   []string `json:"sort"`
-	// TODO: change to ints?
-	SortReverse []bool `json:"sort_reverse"`
-	Limit       uint64 `json:"limit"`
-
-	// Record types (TODO: record struct)
-	PKey   map[string]interface{} `json:"pkey"`
-	Record map[string]interface{} `json:"record"`
-
-	// TODO struct?
-	RecordOp map[string]interface{} `json:"record_op"`
-
-	// TODO; type for the filter itself
-	Filter interface{} `json:"filter"`
-	Join   interface{} `json:"join"`
-}
 
 const (
 	Get    QueryType = "get"
@@ -35,10 +11,47 @@ const (
 	Delete           = "delete"
 	Filter           = "filter"
 
-	// Stream types
+	// Stream types: responses that will return a stream of results
 	FilterStream = "filter_stream"
 )
 
+
+// TODO: func to validate the mix of arguments
+type QueryArgs struct {
+	// Shared options
+	DB            string `json:"db"`
+	Collection    string `json:"collection"`
+	ShardInstance string `json:"shard_instance,omitempty"`
+
+    // Fields defines a list of fields for Projections
+	Fields []string `json:"fields"`
+
+	// Sort + SortReverse control the ordering of results
+	Sort   []string `json:"sort"`
+	// TODO: change to ints?
+	SortReverse []bool `json:"sort_reverse"`
+
+    // Limit is how many records will be returned in the result	
+	Limit       uint64 `json:"limit"`
+	
+	// Record types (TODO: record struct)
+	PKey   map[string]interface{} `json:"pkey"`
+	Record map[string]interface{} `json:"record"`
+
+	// TODO struct?
+	// RecordOp is a map of operations to apply to the record (incr, decr, etc.)
+	RecordOp map[string]interface{} `json:"record_op"`
+
+	// TODO; type for the filter itself
+    // Filter is the conditions to match data on
+	Filter interface{} `json:"filter"`
+
+	// Join defines what data we should pull in addition to the record defined in `Collection`
+	Join   interface{} `json:"join"`
+}
+
+// Query is the struct which contains the entire query to run, this includes
+// both the function to run and the args associated
 type Query struct {
 	Type QueryType
 	Args QueryArgs
