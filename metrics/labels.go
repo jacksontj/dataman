dba8a157e91c46a394d33bbe36dffb580199f4f8
Thomas Jackson <jacksontj.89@gmail.com>
2018-03-27 19:39:29 -0700

    move to labels

--- metrics/labels.go
+++ metrics/labels.go
@@ -18,3 +18,29 @@ func (l LabelSet) String() string {
 	return fmt.Sprintf("{%s}", strings.Join(lstrs, ", "))
 
 }
+
+func MergeLabels(m LabelSet, k []string, v []string) LabelSet {
+	n := make(LabelSet)
+
+	for k, v := range m {
+		n[k] = v
+	}
+
+	for i, kv := range k {
+		n[kv] = v[i]
+	}
+	return n
+}
+
+func MergeLabelsDirect(m LabelSet, o LabelSet) LabelSet {
+	n := make(LabelSet)
+
+	for k, v := range m {
+		n[k] = v
+	}
+
+	for k, v := range o {
+		n[k] = v
+	}
+	return n
+}
