78987fca4555004db18dfe715a25f621625186e9
Thomas Jackson <jacksontj.89@gmail.com>
2018-05-26 15:38:59 -0700

    Add test for metric serialization, and perf improvements

--- metrics/metric.go
+++ metrics/metric.go
@@ -1,8 +1,6 @@
 package metrics
 
-import (
-	"fmt"
-)
+import "strings"
 
 // A metric is defined as (1) name and (2) labelset
 type Metric struct {
@@ -13,5 +11,13 @@ type Metric struct {
 
 // TODO: nicely layout the m.Labels (instead of the go print out)
 func (m Metric) String() string {
-	return fmt.Sprintf("%s%v", m.Name, m.Labels)
+	if len(m.Labels) == 0 {
+		return m.Name
+	}
+
+	labelStrings := make([]string, 0, len(m.Labels))
+	for k, v := range m.Labels {
+		labelStrings = append(labelStrings, k+`="`+v+`"`)
+	}
+	return m.Name + "{" + strings.Join(labelStrings, ", ") + "}"
 }
